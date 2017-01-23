f4f6749b2476830e37d8c5992386b192184230f9
Zhiqi Guo <guozhiqi14@gmail.com>
2017-01-23 22:52:08 +0800

    ex2

---
+++ machine-learning-ex2/ex2/plotData.m
@@ -0,0 +1,30 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+pos1 = find(y==1); %positive
+pos2 = find(y==0); %negative
+
+
+plot(X(pos1,1),X(pos1,2),'k+','LineWidth', 2,'MarkerSize', 7);
+plot(X(pos2,1),X(pos2,2),'ko', 'MarkerFaceColor', 'y','MarkerSize', 7);
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
